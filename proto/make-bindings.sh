6cfff60f215e020a3f217b3fc0cc733e9f13dfa9
Sharon Aicler <saichler@gmail.com>
2025-07-09 11:40:02 -0700

    Add collection protos to polaris

--- proto/make-bindings.sh
+++ proto/make-bindings.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Use the protoc image to run protoc.sh and generate the bindings.
 docker run --user "$(id -u):$(id -g)" -e PROTO=pollaris.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=devices.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
+docker run --user "$(id -u):$(id -g)" -e PROTO=collect.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 
 # Now move the generated bindings to the models directory and clean up
 mkdir -p ../go/types
