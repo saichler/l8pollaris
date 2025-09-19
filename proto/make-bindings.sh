f5ce0af607338c988605110523c608c44c53c01a
Sharon Aicler <saichler@gmail.com>
2025-09-19 08:13:55 -0700

    fix filename

--- proto/make-bindings.sh
+++ proto/make-bindings.sh
@@ -5,7 +5,8 @@ wget https://raw.githubusercontent.com/saichler/l8types/refs/heads/main/proto/se
 docker run --user "$(id -u):$(id -g)" -e PROTO=pollaris.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 docker run --user "$(id -u):$(id -g)" -e PROTO=targets.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
 docker run --user "$(id -u):$(id -g)" -e PROTO=jobs.proto --mount type=bind,source="$PWD",target=/home/proto/ -it saichler/protoc:latest
-rm -rf service.proto
+
+rm -rf ./services.proto
 
 # Now move the generated bindings to the models directory and clean up
 rm -rf ../go/types
